25ddfb840746ac2243ef4c43fcf696b5cd2392c3
Matias Lineros R <58137421+MatiasLineros12@users.noreply.github.com>
2019-12-17 20:02:59 +0100

    valoracion
    controlar que solo se haga una vez la valoracion mediante el controlador

--- zezible/app/Http/Controllers/ValoracionController.php
+++ zezible/app/Http/Controllers/ValoracionController.php
@@ -17,18 +17,35 @@ class ValoracionController extends Controller {
     	$user =  \Auth::user();
     	$usuario = User::findOrFail($user->id);
 
-    	if($usuario->roles->nombre == "Socio"){
+		if($usuario->roles->nombre == "Socio"){
     		$valoracion->socio_id = $usuario->id;
     		$valoracion->estado = "socio-voluntario";
     		$valoracion->voluntario_id = $request->input('apuntado_id');
-    	}
 
+            $socioValoro = Valoracion::where('socio_id', ($usuario->id))
+                                        ->where('actividad_id', ($request->input('actividad_id')))
+                                        ->where('voluntario_id', ($request->input('apuntado_id')))
+                                        ->first();
+
+            if($socioValoro){
+                abort(403, "Ya has valorado a este usuario en esta actividad");
+            }
+          
+    	}
     	if($usuario->roles->nombre == "Voluntario"){
     		$valoracion->voluntario_id = $usuario->id;
     		$valoracion->estado = "voluntario-socio";
     		$valoracion->socio_id = $request->input('apuntado_id');
-    	}
 
+             $voluntarioValoro = Valoracion::where('voluntario_id', ($usuario->id))
+                                        ->where('actividad_id', ($request->input('actividad_id')))
+                                        ->where('socio_id', ($request->input('apuntado_id')))
+                                        ->first();
+
+            if($voluntarioValoro){
+                abort(403, "Ya has valorado a este usuario en esta actividad");
+            }
+    	}
     	$valoracion->actividad_id = $request->input('actividad_id');
     	$valoracion->descripcion = $request->input('descripcion');
     	$valoracion->valoracion = $request->input('estrellas');
