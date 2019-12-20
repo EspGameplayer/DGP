7441d7a878479b1c8a931b7662b025ffdc37c251
Matias Lineros R <58137421+MatiasLineros12@users.noreply.github.com>
2019-12-20 21:46:35 +0100

    cambio valoraciones
    gestor ve las valoraciones de las actividades, mediante numeros y no estrellas

--- zezible/resources/views/Valoraciones.blade.php
+++ zezible/resources/views/Valoraciones.blade.php
@@ -35,34 +35,7 @@
     <div class="card-body">
       <h5 class="card-title">
         
-<!--ESTRELLAS-->
-<div id="estrellass">
-                      
-<p class="clasificacion">
-       <input id="radio1" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 5){echo("checked");} ?>
-        value="5"><!--
-    --><label for="radio1">★</label><!--
-    --><input id="radio2" disabled type="radio" name="estrellas"
-          <?php if($valoracion->valoracion == 4){echo("checked");} ?>
-        value="4"><!--
-    --><label for="radio2">★</label><!--
-    --><input id="radio3" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 3){echo("checked");} ?>
-        value="3"><!--
-    --><label for="radio3">★</label><!--
-    --><input id="radio4" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 2){echo("checked");} ?>
-        value="2"><!--
-    --><label for="radio4">★</label><!--
-    --><input id="radio5" disabled type="radio" name="estrellas"
-          <?php if($valoracion->valoracion == 1){echo("checked");} ?>
-        value="1"><!--
-    --><label for="radio5">★</label>
-  </p>
-
-</div>
-<!--FIN ESTRELLAS-->
+      <span class="dot" style="text-align: center;"><br>{{$valoracion->valoracion}}/5</span>
 
       </h5>
       <p class="card-text">{{$valoracion->descripcion}}</p>
@@ -106,34 +79,7 @@
     <div class="card-body">
       <h5 class="card-title">
         
-<!--ESTRELLAS-->
-<div id="estrellass">
-                      
-<p class="clasificacion">
-       <input id="radio1" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 5){echo("checked");} ?>
-        value="5"><!--
-    --><label for="radio1">★</label><!--
-    --><input id="radio2" disabled type="radio" name="estrellas"
-          <?php if($valoracion->valoracion == 4){echo("checked");} ?>
-        value="4"><!--
-    --><label for="radio2">★</label><!--
-    --><input id="radio3" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 3){echo("checked");} ?>
-        value="3"><!--
-    --><label for="radio3">★</label><!--
-    --><input id="radio4" disabled type="radio" name="estrellas" 
-          <?php if($valoracion->valoracion == 2){echo("checked");} ?>
-        value="2"><!--
-    --><label for="radio4">★</label><!--
-    --><input id="radio5" disabled type="radio" name="estrellas"
-          <?php if($valoracion->valoracion == 1){echo("checked");} ?>
-        value="1"><!--
-    --><label for="radio5">★</label>
-  </p>
-
-</div>
-<!--FIN ESTRELLAS-->
+      <span class="dot" style="text-align: center;"><br>{{$valoracion->valoracion}}/5</span>
 
       </h5>
       <p class="card-text">{{$valoracion->descripcion}}</p>
